974e087f5187b1caa9d510a22a03fbcbb74dc1f2
Yuri Cruger <aoddrewiid@gmail.com>
2024-02-24 10:42:11 -0300

    feat: add second InfiniteMovingCards

--- src/components/Achievements/index.tsx
+++ src/components/Achievements/index.tsx
@@ -1,4 +1,7 @@
-import { PROJECTS_CARDS } from "@/constants/projectsCards";
+import {
+  PROJECTS_CARDS_FIRST,
+  PROJECTS_CARDS_SECOND,
+} from "@/constants/projectsCards";
 import { SectionTitle } from "../SectionTitle";
 import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
 
@@ -6,13 +9,18 @@ export function Projects() {
   return (
     <section
       id="projects"
-      className="flex min-h-screen flex-col bg-slate-600/30 py-10"
+      className="flex min-h-screen flex-col bg-slate-600/30 py-10 dark:bg-zinc-800"
     >
-      <div>
-        <SectionTitle title="Projects" />
+      <SectionTitle title="Projects" />
+      <div className="flex flex-1 flex-col justify-center">
         <InfiniteMovingCards
-          items={PROJECTS_CARDS}
+          items={PROJECTS_CARDS_FIRST}
           direction="right"
+          speed="normal"
+        />
+        <InfiniteMovingCards
+          items={PROJECTS_CARDS_SECOND}
+          direction="left"
           speed="normal"
         />
       </div>
