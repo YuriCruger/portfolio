b48112522e6c240b86ae8bbb5f70b68e4475e4f4
Yuri Cruger <aoddrewiid@gmail.com>
2024-02-28 17:43:41 -0300

    style: new background color to CardSkill component

--- src/components/AboutSection/components/CardSkill/index.tsx
+++ src/components/AboutSection/components/CardSkill/index.tsx
@@ -2,7 +2,7 @@ import { TechProps } from "@/constants/techs";
 
 export function CardSkill({ title, icon, time }: TechProps) {
   return (
-    <div className="flex flex-col gap-2 rounded-lg bg-zinc-500/90 p-3 text-zinc-300 shadow shadow-zinc-700 transition-all hover:text-zinc-700 dark:bg-zinc-950/20 dark:text-zinc-500 dark:hover:text-zinc-400 sm:p-6">
+    <div className="flex flex-col gap-2 rounded-lg bg-zinc-600/80 p-3 text-zinc-300 shadow shadow-zinc-700 transition-all hover:text-zinc-700 dark:bg-zinc-600/40 dark:text-zinc-500 dark:hover:text-zinc-400 sm:p-6">
       <div className="flex items-center justify-between">
         <p className="font-medium">{title}</p>
         <div>{icon}</div>
