d767805fc30afcce1401dc4034dc3a2a676785f9
Yuri Cruger <aoddrewiid@gmail.com>
2024-02-28 17:34:18 -0300

    refactor: improve ScrollToTop component

--- src/components/ScrollToTop/index.tsx
+++ src/components/ScrollToTop/index.tsx
@@ -1,32 +1,30 @@
 "use client";
 
 import { ArrowUpIcon } from "lucide-react";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export function ScrollToTop() {
-  const [show, setShow] = useState(false);
+  const [isButtonVisible, setIsButtonVisible] = useState(false);
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  const handleScroll = useCallback(() => {
-    if (!show && window.scrollY > 500) {
-      setShow(true);
-    }
-    if (show && window.scrollY < 500) {
-      setShow(false);
-    }
-  }, [show]);
-
   useEffect(() => {
+    const handleScroll = () => {
+      const shouldShow = window.scrollY > 500;
+      if (isButtonVisible !== shouldShow) {
+        setIsButtonVisible(shouldShow);
+      }
+    };
+
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [handleScroll]);
+  }, [isButtonVisible]);
 
   return (
     <div>
-      {show && (
+      {isButtonVisible && (
         <div className="fixed bottom-4 right-4 z-50 h-10 w-10 rounded-full bg-zinc-800 p-2 text-white shadow-md dark:bg-zinc-700">
           <button onClick={scrollToTop}>
             <ArrowUpIcon />
