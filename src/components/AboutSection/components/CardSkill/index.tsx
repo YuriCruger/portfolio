7932dcc33fceb1278c31309362574e4cf5456eab
Yuri Cruger <aoddrewiid@gmail.com>
2024-02-29 08:54:25 -0300

    refactor: remove hover effect from cardSKill

--- src/components/AboutSection/components/CardSkill/index.tsx
+++ src/components/AboutSection/components/CardSkill/index.tsx
@@ -2,7 +2,7 @@ import { TechComponent, TechProps } from "@/constants/techs";
 
 export function CardSkill({ title, icon, startDate }: TechProps) {
   return (
-    <div className="flex flex-col gap-2 rounded-lg bg-zinc-600/80 p-3 text-zinc-300 shadow shadow-zinc-700 transition-all hover:text-zinc-700 dark:bg-zinc-600/40 dark:text-zinc-500 dark:hover:text-zinc-400 sm:p-6">
+    <div className="flex flex-col gap-2 rounded-lg bg-zinc-600/80 p-3 text-zinc-300 shadow shadow-zinc-700 transition-all dark:bg-zinc-600/40 dark:text-zinc-500 sm:p-6">
       <div className="flex items-center justify-between">
         <p className="font-medium">{title}</p>
         <div>{icon}</div>
